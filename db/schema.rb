523d03c6dfa82369948be109e8577f57d32fb04e
Yoshida <yoshida@primebrains.co.jp>
2021-07-20 23:49:27 +0900

    add colomn and database settings

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_19_143420) do
+ActiveRecord::Schema.define(version: 2021_07_20_143450) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "text"
   end
 
 end
